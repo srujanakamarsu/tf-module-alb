567bbcbe85973050e82111a0a1b62c928a9fc587
srujanakamarsu <srujanamythree@gmail.com>
2023-09-22 21:21:53 +0530

    alb

--- main.tf
+++ main.tf
@@ -1,10 +1,26 @@
 resource "aws_lb" "main" {
-  name               = "${var.env}-alb"
+  name               = local.lb_name
   internal           = var.internal
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
   tags               = merge(local.tags, { Name = "${var.env}-alb" })
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "404"
+    }
+  }
 }
 
 resource "aws_security_group" "main" {
